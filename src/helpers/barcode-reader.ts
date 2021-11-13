bbb56667c65fe01f7aab4dd528c4ef3be1537e55
mkanenobu <mitsuru.kanenobu@gmail.com>
2021-11-13 18:34:48 +0900

    Fix on BarcodeDetector unavailable

--- src/helpers/barcode-reader.ts
+++ src/helpers/barcode-reader.ts
@@ -1,9 +1,16 @@
-export const initQrCodeDetector = (): any => {
+/**
+ * Get BarcodeDetector instance
+ *
+ * @throws Error
+ */
+export const getQrCodeDetector = (): Record<string, any> | undefined => {
   if ("BarcodeDetector" in window) {
     // create new detector
     // @ts-expect-error
     return new BarcodeDetector({
       formats: ["qr_code"],
     });
+  } else {
+    throw new Error("BarcodeDetector api is not available");
   }
 };
