599749c93307c0d438a74f8119407def6589bd98
mkanenobu <mitsuru.kanenobu@gmail.com>
2022-06-05 15:57:02 +0900

    misc

--- src/components/QrCodeReader.tsx
+++ src/components/QrCodeReader.tsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { type OnResultFunction, QrReader } from "react-qr-reader";
+import isMobile from "ismobilejs";
 
-export const QrCodeReader: React.FC<{ onResult: OnResultFunction }> = ({
-  onResult,
-}) => {
+interface QrCodeReaderProps {
+  onResult: OnResultFunction;
+}
+
+export const QrCodeReader: React.FC<QrCodeReaderProps> = ({ onResult }) => {
+  const mobile = isMobile(window.navigator).any;
+
   return (
     <QrReader
       onResult={onResult}
-      constraints={{ aspectRatio: 16 / 12 }}
-      containerStyle={{ minWidth: "80%" }}
-      videoContainerStyle={{ width: "100%" }}
-      videoStyle={{ width: "100%" }}
+      constraints={{
+        aspectRatio: mobile ? 1 : 1.6,
+        facingMode: mobile ? { exact: "environment" } : "user",
+      }}
+      containerStyle={{ maxHeight: "80vh" }}
+      videoContainerStyle={{ width: "100%", maxHeight: "80vh" }}
+      videoStyle={{ width: "100%", maxHeight: "80vh" }}
     />
   );
 };
